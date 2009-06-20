2c46d01b0ce96bd93e9704559be92d313a55e034
rnakhan <rnakhan@4f78f21a-e313-497e-ae84-9e4a1d45898a>
2009-06-20 00:16:53 +0000

    wip PE installer

--- sipper/version.rb
+++ sipper/version.rb
@@ -2,7 +2,7 @@ module SIP
   module VERSION #:nodoc:
     unless defined? MAJOR
       MAJOR = 2
-      MINOR = 1
+      MINOR = 0
       TINY  = 0
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
